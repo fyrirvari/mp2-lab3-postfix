5dd85b197acdc7b8a9e6438513e4b7b961e12c61
fyrirvari <klevtsov.n@mail.ru>
2020-10-22 21:56:23 +0300

    supplemented stack implementation & added test for the stack

--- Klevtsov/base/stack.h
+++ Klevtsov/base/stack.h
@@ -1,44 +1,121 @@
 ﻿#ifndef __STACK_H__
 #define __STACK_H__
 
-const int MaxStackSize = 100;
+const int MAX_STACK_SIZE = 100;
 
 template <class T>
 class TStack
 {
   T *pMem;
-  int size;
+  int capacity;
   int top;
 public:
-  TStack(int _size)
+  TStack(int _capacity = 10) : capacity(_capacity), top(-1)
   {
-    size = _size;
-    top = -1;
-    if ((size < 1) || (size > MaxStackSize))
-      throw size;
-    pMem = new T[size];
+    if ((capacity < 1) || (capacity > MAX_STACK_SIZE))
+      throw capacity;
+    pMem = new T[capacity];
   }
+
+  TStack(const TStack& st) : capacity(st.capacity), top(st.top)
+  {
+	  pMem = new T[capacity];
+	  for (int i = 0; i <= top; ++i)
+		  pMem[i] = st.pMem[i];
+  }
+
   ~TStack()
   {
     delete [] pMem;
   }
+
+  inline int size() const
+  {
+	  return top + 1;
+  }
+
   inline bool empty() const
   {
 	  return top == -1;
   }
+
   inline bool full() const
   {
-	  return top == size - 1;
+	  return top == MAX_STACK_SIZE - 1;
   }
+
   void push(const T& value)
   {
-	  if (this->full()) throw std::exception("Stack is full");
+	  if (full()) throw std::exception("Stack overflow");
+	  if (capacity - 1 == top)
+	  {
+		  int tmpCapacity = capacity * 2 > MAX_STACK_SIZE ? MAX_STACK_SIZE : capacity * 2;
+		  T* tmp = new T[tmpCapacity];
+		  for (int i = 0; i < capacity; ++i)
+			  tmp[i] = pMem[i];
+		  delete[] pMem;
+		  capacity = tmpCapacity;
+		  pMem = tmp;
+	  }
 	  pMem[++top] = value;
   }
+
   T pop()
   {
-	  if (this->empty()) throw std::exception("Stack is empty");
+	  if (empty()) throw std::exception("Stack underflow");
 	  return pMem[top--];
+  }
+
+  T peek() const
+  {
+	  if (empty()) throw std::exception("Stack underflow");
+	  return pMem[top];
+  }
+
+  void clear()
+  {
+	  top = -1;
+  }
+
+  int search(const T& value) const
+  {
+	  int i, qty;
+	  for (i = top, qty = 0; i >= 0; --i)
+	  {
+		  if (pMem[i] == value) break;
+		  else ++qty;
+	  }
+	  return i == -1 ? -1 : qty;
+  }
+
+  TStack& operator=(const TStack& st)
+  {
+	  if (this != &st)
+	  {
+		  delete[] pMem;
+		  capacity = st.capacity;
+		  top = st.top;
+		  pMem = new T[capacity];
+		  for (int i = 0; i <= top; ++i)
+			  pMem[i] = st.pMem[i];
+	  }
+	  return *this;
+  }
+
+  bool operator==(const TStack& st) const
+  {
+	  if (this != &st)
+	  {
+		  if (top != st.top) return false;
+		  for (int i = 0; i <= top; ++i)
+			  if (pMem[i] != st.pMem[i]) return false;
+	  }
+	  return true;
+  }
+
+  bool operator!=(const TStack& st) const
+  {
+	  return !(*this == st);
   }
 };
 
--- Klevtsov/base_test/test_tstack.cpp
+++ Klevtsov/base_test/test_tstack.cpp
@@ -5,3 +5,174 @@ TEST(TStack, can_create_stack_with_positive_length)
 {
   ASSERT_NO_THROW(TStack<int> st(5));
 }
+
+TEST(TStack, cant_create_stack_with_too_large_length)
+{
+	ASSERT_ANY_THROW(TStack<int> st(MAX_STACK_SIZE + 1));
+}
+
+TEST(TStack, can_create_stack_with_negative_length)
+{
+	ASSERT_ANY_THROW(TStack<int> st(-5));
+}
+
+TEST(TStack, can_create_copied_stack)
+{
+	TStack<int> st(5);
+	ASSERT_NO_THROW(TStack<int> st1(st));
+}
+
+TEST(TStack, copied_stack_is_equal_to_source_one)
+{
+	TStack<int> st(5);
+	for (int i = 0; i < 5; ++i)
+		st.push(i);
+	TStack<int> st1(st);
+	EXPECT_EQ(st, st1);
+}
+
+TEST(TStack, can_get_size)
+{
+	TStack<int> st;
+	EXPECT_EQ(true, st.empty());
+	for (int i = 0; i < 5; ++i)
+		st.push(i);
+	EXPECT_EQ(5, st.size());
+}
+
+TEST(TStack, can_get_boolean_if_the_stack_is_full)
+{
+	TStack<int> st(MAX_STACK_SIZE);
+	EXPECT_EQ(false, st.full());
+	for (int i = 0; i < MAX_STACK_SIZE; ++i)
+		st.push(i);
+	EXPECT_EQ(true, st.full());
+}
+
+TEST(TStack, can_get_boolean_if_the_stack_is_empty)
+{
+	TStack<int> st;
+	EXPECT_EQ(true, st.empty());
+	st.push(0);
+	EXPECT_EQ(false, st.empty());
+}
+
+TEST(TStack, can_push_and_pop_element)
+{
+	TStack<int> st;
+	ASSERT_NO_THROW(st.push(10));
+	EXPECT_EQ(10, st.pop());
+}
+
+TEST(TStack, can_peek_element)
+{
+	TStack<int> st;
+	ASSERT_NO_THROW(st.push(10));
+	EXPECT_EQ(10, st.peek());
+}
+
+TEST(TStack, cant_push_element_when_stack_is_overflow)
+{
+	TStack<int> st(MAX_STACK_SIZE);
+	for (int i = 0; i < MAX_STACK_SIZE; ++i)
+		st.push(i);
+	ASSERT_ANY_THROW(st.push(10));
+}
+
+TEST(TStack, cant_pop_element_when_stack_is_underflow)
+{
+	TStack<int> st;
+	ASSERT_ANY_THROW(st.pop());
+}
+
+TEST(TStack, cant_search_a_nonexistent_item)
+{
+	TStack<int> st;
+	for (int i = 0; i < 10; ++i)
+		st.push(i);
+	EXPECT_EQ(-1, st.search(10));
+}
+
+TEST(TStack, can_search_an_existing_item)
+{
+	TStack<int> st;
+	for (int i = 0; i < 10; ++i)
+		st.push(i);
+	int qty = st.search(3);
+	for (int i = 0; i < qty; ++i)
+		st.pop();
+	EXPECT_EQ(3, st.peek());
+}
+
+TEST(TStack, can_clear_stack)
+{
+	TStack<int> st;
+	for (int i = 0; i < 10; ++i)
+		st.push(i);
+	st.clear();
+	EXPECT_EQ(true, st.empty());
+}
+
+
+TEST(TStack, can_assign_stack_to_itself)
+{
+	TStack<int> st;
+	ASSERT_NO_THROW(st = st);
+}
+
+TEST(TStack, can_assign_stacks_of_equal_size)
+{
+	TStack<int> st;
+	TStack<int> st1;
+	EXPECT_EQ(st.size(), st1.size());
+	for (int i = 0; i < 5; i++)
+		st.push(i);
+	EXPECT_EQ(st, st1 = st);
+}
+
+TEST(TStack, assign_operator_change_stack_size)
+{
+	TStack<int> st;
+	TStack<int> st1;
+	st.push(0);
+	EXPECT_NE(st.size(), st1.size());
+	st = st1;
+	EXPECT_EQ(st.size(), st1.size());
+}
+
+TEST(TStack, can_assign_stacks_of_different_size)
+{
+	TStack<int> st;
+	TStack<int> st1;
+	for (int i = 0; i < 10; i++)
+		st1.push(i);
+	EXPECT_NE(st.size(), st1.size());
+	EXPECT_EQ(st1, st = st1);
+}
+
+
+TEST(TStack, compare_equal_stacks_return_true)
+{
+	TStack<int> st;
+	for (int i = 0; i < 10; i++)
+		st.push(i);
+	TStack<int> st1(st);
+	EXPECT_EQ(true, st1 == st);
+}
+
+TEST(TStack, compare_stack_with_itself_return_true)
+{
+	TStack<int> st;
+	for (int i = 0; i < 10; i++)
+		st.push(i);
+	EXPECT_EQ(true, st == st);
+}
+
+TEST(TStack, stacks_with_different_size_are_not_equal)
+{
+	TStack<int> st;
+	TStack<int> st1;
+	st.push(0);
+	EXPECT_NE(st.size(), st1.size());
+	EXPECT_EQ(true, st != st1);
+}
